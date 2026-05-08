2d4076b4755990c5763485180cb46fbc42438416
Felipe Miranda <elabogadogeek1@gmail.com>
2026-05-08 12:09:06 -0400

    agregar ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,65 @@
+let nombre = "felipe";
+let apellido = "Miranda";
+console.log(nombre ,  apellido);
+
+let numero1 = 10; 
+let numero2 = 5;
+let numero3 = 3; 
+console.log((numero1 + numero2) * numero3);
+
+let comida1 = "lasaña";
+console.log("Me gusta la " + comida1);
+
+let a = 10;
+let b = 3; 
+
+console.log(a + b);
+console.log(a - b);  //operador de resta, devuelve la diferencia entre a y b
+console.log(a * b);  //operador de multiplicación, devuelve el producto de a y b
+console.log(a % b);  //operador de division, devuelve el cociente de a y b
+
+//continuar
+a = "10";
+b = "3"; 
+console.log(a + b);
+console.log(a - b);  //operador de resta, devuelve la diferencia entre a y b
+console.log(a * b);  //operador de multiplicación, devuelve el producto de a y b
+console.log(a % b);
+
+
+let edad1 = 15;
+let edad2 = 10; 
+let edad3 = 18;  
+console.log(edad1 > edad2); //mayor que
+console.log(edad1 < edad3); //menor que
+
+// ejercicio 6 
+let number4 = 8; 
+let number5 = 5; 
+let number6 = 10; 
+console.log (number4 > number5);
+console.log (number4 < number6);
+
+// ejercicio 7
+let number7 = 6; 
+let number8 = 7; 
+console.log (number7 === number8); 
+
+//ejercicio 8
+
+let temperatura = 25;
+let temperatura2 = 30;
+console.log(temperatura >= temperatura2); //true
+
+//ejercicio 9
+
+//ejercicio 10
+let edad=17;
+let tienePermiso=true;
+console.log(edad >= 18 || tienePermiso); //false
+
+//ejercicio 11
+
+let usuario="admin";
+let password="1234";
+console.log (usuario === "admin" && password === "1234"); //true
